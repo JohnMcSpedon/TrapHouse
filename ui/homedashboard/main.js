39cd9750a7bd23d7139985a6ab258504ed711aec
Rob Sami <rob@benchling.com>
2016-07-11 00:23:59 -0700

    clean up

--- ui/homedashboard/main.js
+++ ui/homedashboard/main.js
@@ -2,32 +2,12 @@ import {
   AppRegistry,
   Text,
   View,
-  Button,
   TouchableHighlight,
 } from 'react-native';
 
 import {Facebook} from 'exponent';
 import React from 'react';
 
-async function logIn() {
-  try {
-    const { type, token } = await Facebook.logInWithReadPermissionsAsync(
-      '100506880388601', {
-        permissions: ['public_profile'],
-      });
-  } catch (e) {
-    console.log('fuck! ', e);
-  }
-  if (type === 'success') {
-    // Get the user's name using Facebook's Graph API
-    const response = await fetch(
-      `https://graph.facebook.com/me?access_token=${token}`);
-    Alert.alert(
-      'Logged in!',
-      `Hi ${(await response.json()).name}!`,
-    );
-  }
-}
 
 const App = React.createClass ({
 
